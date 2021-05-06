d69686d55c83d1663ee542634219165283155409
ciberpipa <luis.perez@insbaixcamp.cat>
2021-05-06 16:10:19 +0200

    proyecto illustrateme
    jaja

---
+++ database/factories/GuiaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Guia;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GuiaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Guia::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
